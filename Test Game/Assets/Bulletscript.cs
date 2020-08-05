852e067fe4c503f6a98f0579de0fc1f5a65fcdb8
MeowMeowCats <69171074+MeowMeowCats@users.noreply.github.com>
2020-08-05 14:33:16 -0700

    mewo mewo

--- Test Game/Assets/Bulletscript.cs
+++ Test Game/Assets/Bulletscript.cs
@@ -11,7 +11,7 @@ public class Bulletscript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(this.gameObject, 5);
+        Destroy(this.gameObject, 3);
     }
 
     // Update is called once per frame
