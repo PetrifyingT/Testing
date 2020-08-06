20e28e0707c5673bc8facea2ff881b36947f2b9a
MeowMeowCats <69171074+MeowMeowCats@users.noreply.github.com>
2020-08-06 13:09:14 -0700

    afsafdafafd

--- Test Game/Assets/Playercontrols.cs
+++ Test Game/Assets/Playercontrols.cs
@@ -8,6 +8,7 @@ public class Playercontrols : MonoBehaviour
     public GameObject cameraY;
     bool canJump = true;
     Rigidbody rb;
+    public int jumpForce = 20;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +21,8 @@ public class Playercontrols : MonoBehaviour
     {
         if (Input.GetButtonDown("Jump") && canJump == true)
         {
-
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            canJump = false;
         } 
         if (Input.GetKey(KeyCode.W)) transform.Translate(Vector3.forward * speed * Time.deltaTime);
         if (Input.GetKey(KeyCode.D)) transform.Translate(Vector3.right * speed * Time.deltaTime);
@@ -30,6 +32,12 @@ public class Playercontrols : MonoBehaviour
         cameraY.transform.Rotate(-3* Input.GetAxis("Mouse Y") , 0, 0);
 
     }
-
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("ground"))
+        {
+            canJump = true;
+        }
+    }
 
 }
